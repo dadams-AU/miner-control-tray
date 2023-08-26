ff2c56f945b34e46623abb31a5221853d1265e7c
dadams-AU <dpadams@fullerton.edu>
2023-08-25 18:29:12 -0700

    first

---
+++ xna.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import schedule
+import os
+import threading
+from datetime import datetime, time
+from time import sleep
+
+os.system('clear')
+
+keep_running = True  # Shared flag to control the scheduler thread
+
+
+def start_miner():
+    current_time = datetime.now().time()
+    if current_time <= time(17, 0) or current_time >= time(20, 0):
+        os.chdir('/home/miner/miner/t-rex/')
+        os.system('./xna.sh')
+    else:
+        print("It's not the scheduled time to start the miner. Wait until 8 PM.")
+
+
+def stop_miner():
+    os.system('pkill -f t-rex')
+
+
+schedule.every().day.at("20:00").do(start_miner)
+schedule.every().day.at("17:00").do(stop_miner)
+
+
+def scheduler():
+    global keep_running
+    while keep_running:
+        schedule.run_pending()
+        sleep(60)
+
+
+t = threading.Thread(target=scheduler)
+t.start()
+
+print("T-Rex Miner Controller")
+print("Press (k) to manually kill the miner.")
+print("Press (s) to manually start the miner.")
+print("Press (q) to quit this script.")
+
+while True:
+    choice = input("\n\nPlease enter your choice and press Enter: ")
+
+    if choice == 'k':
+        stop_miner()
+    elif choice == 's':
+        start_miner()
+    elif choice == 'q':
+        stop_miner()
+        keep_running = False  # Signal the scheduler to stop
+        t.join()  # Wait for the scheduler thread to finish
+        break
---
+++ xna_app.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import pystray
+from PIL import Image, ImageDraw
+import schedule
+import os
+import threading
+from datetime import datetime, time
+from time import sleep
+from plyer import notification
+
+
+# Miner control functions
+def start_miner():
+    current_time = datetime.now().time()
+    if current_time <= time(17, 0) or current_time >= time(20, 0):
+        os.chdir('/home/miner/miner/t-rex/')
+        os.system('./xna.sh')
+    else:
+        notification.notify(
+            title="Miner Control",
+            message="It's not the scheduled time to start the miner. Wait until 8 PM.",
+            timeout=10
+        )
+
+def stop_miner():
+    os.system('pkill -f t-rex')
+
+schedule.every().day.at("20:00").do(start_miner)
+schedule.every().day.at("17:00").do(stop_miner)
+
+keep_running = True
+
+def scheduler():
+    while keep_running:
+        schedule.run_pending()
+        sleep(60)
+
+t = threading.Thread(target=scheduler)
+t.start()
+
+# Create system tray icon
+def create_image():
+    width, height = 64, 64
+    color1 = (0, 0, 0)
+    color2 = (255, 255, 255)
+    image = Image.new('RGB', (width, height), color1)
+    dc = ImageDraw.Draw(image)
+    dc.rectangle((width // 2, 0, width, height // 2), fill=color2)
+    dc.rectangle((0, height // 2, width // 2, height), fill=color2)
+    return image
+
+def on_activate_start(icon, item):
+    start_miner()
+
+def on_activate_stop(icon, item):
+    stop_miner()
+
+def on_activate_exit(icon, item):
+    stop_miner()
+    keep_running = False
+    t.join()
+    icon.stop()
+
+image = create_image()
+menu = (pystray.MenuItem('Start Miner', on_activate_start),
+        pystray.MenuItem('Stop Miner', on_activate_stop),
+        pystray.MenuItem('Exit', on_activate_exit))
+icon = pystray.Icon("miner_control", image, "Miner Control", menu)
+icon.run()
